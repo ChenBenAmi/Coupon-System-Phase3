18224ec64a98ad393b72f06852ce864d3d0e415e
Adiivgi <46651329+Adiivgi@users.noreply.github.com>
2019-09-30 20:10:52 +0300

    Company class jpa complete

--- src/main/java/com/project/CouponSystem/beans/Company.java
+++ src/main/java/com/project/CouponSystem/beans/Company.java
@@ -1,5 +1,55 @@
 package com.project.CouponSystem.beans;
 
-public class Company {
+import java.util.Hashtable;
+import java.util.Map;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Company {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+	private String compName;
+	private String password;
+	private String email;
+	private Map<Long, Coupon> couponsCollection = new Hashtable<>();
+	
+	public long getId() {
+		return id;
+	}
+	@Column(unique = true,nullable=false)
+	public String getCompName() {
+		return compName;
+	}
+	@Column(nullable = false)
+	public String getPassword() {
+		return password;
+	}
+	@Column(nullable= false) 
+	public String getEmail() {
+		return email;
+	}
+	@ManyToMany
+	public Map<Long, Coupon> getCouponsCollection() {
+		return couponsCollection;
+	}
+	
+	
+	
 }
