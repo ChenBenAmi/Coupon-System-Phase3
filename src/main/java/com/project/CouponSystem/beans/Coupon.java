521cf71447c4ee716917b4cdfb5907f331806bf2
Chen <chen24201@gmail.com>
2019-09-30 20:10:24 +0300

    company and copoun beans

--- src/main/java/com/project/CouponSystem/beans/Coupon.java
+++ src/main/java/com/project/CouponSystem/beans/Coupon.java
@@ -1,5 +1,62 @@
 package com.project.CouponSystem.beans;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Coupon {
 
+	
+	private long id;
+	private String title;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private int amount;
+	private CouponType couponType;
+	private String message;
+	private double price;
+	private String image;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	@Column
+	public String getTitle() {
+		return title;
+	}
+	@Column
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+	@Column
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+	@Column
+	public int getAmount() {
+		return amount;
+	}
+	@Column
+	public CouponType getCouponType() {
+		return couponType;
+	}
+	@Column
+	public String getMessage() {
+		return message;
+	}
+	@Column
+	public double getPrice() {
+		return price;
+	}
+	@Column
+	public String getImage() {
+		return image;
+	}
+	
+	
 }
